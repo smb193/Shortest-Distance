9ca8ff5f21f6deaf7d6296e6b7db50b171b8881f
smb193 <smb193@zips.uakron.edu>
2015-04-17 15:01:26 -0400

    Added main
    Defined primary datatypes for 2d and 3d

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#define PAIR std::pair<int, int>
+#define V_PAIR std::vector<PAIR>
+
+#define TRIP std::tuple<int, int, int>
+#define V_TRIP std::vector<TRIP>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
