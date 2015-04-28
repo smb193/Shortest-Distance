f0df8900cbd28650f6fbb232c2c1ee2d0df2da00
smb193 <smb193@zips.uakron.edu>
2015-04-28 01:52:42 -0400

    Enhanced readability
    Because that last commit was impossible to follow

--- Distance.cpp
+++ Distance.cpp
@@ -39,19 +39,21 @@ std::pair<PAIR, PAIR> ClosePoints(V_PAIR set)
 
 std::pair<TRIP, TRIP> ClosePoints(V_TRIP set)
 {
-	std::pair<TRIP, TRIP> points;
+	std::pair<TRIP, TRIP> points = std::make_pair(set[0], set[1]);
 	double close = pt_distance(set[0], set[1]);
-
+	if (set.size() == 2)
+		return points;
 	for (int i = 0; i < set.size(); ++i)
 	{
 		if (i != set.size() - 1){
 			for (int j = i + 1; j < set.size(); ++j)
 			{
-				if (abs(std::get<0>(set[i]) - std::get<0>(set[j]))> close)
-					break;
-				if (pt_distance(set[i], set[j]) < close && pt_distance(set[i], set[j]) != 0)
+//				if ((std::get<0>(set[i]) - std::get<0>(set[j])) > close)
+//					break;
+				double dist = pt_distance(set[i], set[j]);
+				if (dist < close && dist != 0)
 				{
-					close = pt_distance(set[i], set[j]);
+					close = dist;
 					points.first = set[i];
 					points.second = set[j];
 				}
@@ -65,24 +67,44 @@ std::pair<TRIP, TRIP> ClosePoints(V_TRIP set)
 
 std::pair<PAIR, PAIR> DividePoints(V_PAIR& set)
 {
+	// This algorithm runs in O(nlogn) time.
+	// It would be faster if it was passed a vector of the same points sorted by Y values.
+	// Instead, this algorithm simplifies slightly by only taking a vector sorted by X values.
+	// T(n) = 2T(n/2) + O(n) ==> O(nlogn)
+
+	// Base case takes 0 comparisons (2 points) or 3 comparisons (3 points). O(1) time.
 	if (set.size() <= 3)
 		return ClosePoints(set);
+
+	// Make copies of left and right sides for recursion.
 	int size = set.size();
+	int middle = set[size / 2].first;
 	V_PAIR left_set, right_set;
 	std::copy(set.begin(), set.begin() + (size / 2), std::back_inserter(left_set));
 	std::copy(set.begin() + (size / 2), set.end(), std::back_inserter(right_set));
-	int middle = set[size / 2].first;
+
+	// Recursively find min distance points from left and right subsets. Two subproblems of size n/2.
 	std::pair<PAIR, PAIR> left_pair = DividePoints(left_set);
 	std::pair<PAIR, PAIR> right_pair = DividePoints(right_set);
 	double left_dist = pt_distance(left_pair.first, left_pair.second);
 	double right_dist = pt_distance(right_pair.first, right_pair.second);
+
+	// Set minimum values based on results.
 	std::pair<PAIR, PAIR> min_pair = (left_dist < right_dist) ? left_pair : right_pair;
 	double strip_dist = std::fmin(left_dist, right_dist);
+
+	// Change from sorting on X to sorting on Y (in order to work within the strip around the median line). O(nlogn) time.
 	std::sort(set.begin(), set.end(), SortPairY);
+
+	// Copy values in the strip if they are within strip_dist of the median.
 	V_PAIR strip;
 	std::copy_if(set.begin(), set.end(), std::back_inserter(strip), [&middle, &strip_dist](const PAIR& p)
 		{ return abs(middle - p.first) < strip_dist; });
+	
 	double min_dist = strip_dist;
+	
+	// The inner loop will run a maximum of 6 times.
+	// Hence checking the strip takes O(n) time -- specifically, T(n) = 6n.
 	for (int i = 0; i < strip.size() - 1; ++i)
 		for (int j = i + 1; j < strip.size() &&
 			((strip[j].second - strip[i].second) < strip_dist); ++j)
